67dbd57932d4413e09967a3da0aa9e452b6f43e0
Rahul Pradev <rahulmysterio1212@gmail.com>
2022-05-02 10:41:17 +0530

    Initial project setup

---
+++ SmileMeatsNetworkKit/SmileMeatsNetworkKit.h
@@ -0,0 +1,18 @@
+//
+//  SmileMeatsNetworkKit.h
+//  SmileMeatsNetworkKit
+//
+//  Created by RAHUL PRADEV on 02/05/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SmileMeatsNetworkKit.
+FOUNDATION_EXPORT double SmileMeatsNetworkKitVersionNumber;
+
+//! Project version string for SmileMeatsNetworkKit.
+FOUNDATION_EXPORT const unsigned char SmileMeatsNetworkKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SmileMeatsNetworkKit/PublicHeader.h>
+
+
